bda93ef9c80a96317147e5d0b5de53b1708fa3d2
AngelLilz <a.ardiz76@gmail.com>
2024-08-18 19:09:54 +0800

    Added temperature function

--- src/script.js
+++ src/script.js
@@ -1,6 +1,11 @@
 function refreshWeather (response) {
     let temperatureElement = document.querySelector("#temperature");
- temperatureElement.innerHTML = response.data.temperature.current;
+ let temperature = response.data.temperature.current;
+ let cityElement = document.querySelector("#city");
+
+
+ cityElement.innerHTML = response.data.city;
+ temperatureElement.innerHTML = Math.round(temperature);
 }
 
 
@@ -17,8 +22,8 @@ axios.get(apiUrl).then(refreshWeather);
 function handleSearchSubmit(event){
 event.preventDefault();
 let searchInput = document.querySelector("#search-form-input");
-let cityElement = document.querySelector("#city");
-cityElement.innerHTML = searchInput.value;
+
+
 searchCity(searchInput.value);
 
 
@@ -31,3 +36,5 @@ searchCity(searchInput.value);
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Perth");
