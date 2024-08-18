88d43673a75c4aaf13da798025c6648b52940105
AngelLilz <a.ardiz76@gmail.com>
2024-08-18 17:42:43 +0800

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,8 +1,10 @@
 function handleSearchSubmit(event){
 event.preventDefault();
 let searchInput = document.querySelector("#search-form-input");
-let cityElement = document.querySelector("#city");
-cityElement = searchInput.value;
+console.log(searchInput.value);
+
+
+
 }
 
 
