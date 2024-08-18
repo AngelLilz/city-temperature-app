de4f0710b39ee2bfb57d9dee920c68cc31f571ba
AngelLilz <a.ardiz76@gmail.com>
2024-08-18 21:43:23 +0800

    Injected conditions

--- src/script.js
+++ src/script.js
@@ -2,10 +2,17 @@ function refreshWeather (response) {
     let temperatureElement = document.querySelector("#temperature");
  let temperature = response.data.temperature.current;
  let cityElement = document.querySelector("#city");
+ let descriptionElement = document.querySelector("#description");
+ let humidityElement = document.querySelector("#humidity");
+ let windSpeedElement = document.querySelector("#wind-speed");
 
+console.log(wind);
 
  cityElement.innerHTML = response.data.city;
- temperatureElement.innerHTML = Math.round(temperature);
+ descriptionElement.innerHTML = response.data.condition.description;
+
+humidityElement.innerHTML = `${response.data.condition.humidity}%`;
+temperatureElement.innerHTML = Math.round(temperature);
 }
 
 
