47a50297785f927903654f7a56c3598dfe3c7cef
AngelLilz <a.ardiz76@gmail.com>
2024-08-18 18:55:34 +0800

    Added temperature script

--- src/script.js
+++ src/script.js
@@ -1,8 +1,25 @@
+function refreshWeather (response) {
+    let temperatureElement = document.querySelector("#temperature");
+ temperatureElement.innerHTML = response.data.temperature.current;
+}
+
+
+
+function searchCity(city) {
+let apiKey = "5a3530d402b8t82b0332df19d81od2ad";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+axios.get(apiUrl).then(refreshWeather);
+}
+
+
+
+
 function handleSearchSubmit(event){
 event.preventDefault();
 let searchInput = document.querySelector("#search-form-input");
 let cityElement = document.querySelector("#city");
 cityElement.innerHTML = searchInput.value;
+searchCity(searchInput.value);
 
 
 
