e9e792b4705f422ce17140cdee0b7119354fcaea
AngelLilz <a.ardiz76@gmail.com>
2024-08-18 22:55:49 +0800

    Injected temp icon and time and windspeed

--- src/script.js
+++ src/script.js
@@ -5,14 +5,40 @@ function refreshWeather (response) {
  let descriptionElement = document.querySelector("#description");
  let humidityElement = document.querySelector("#humidity");
  let windSpeedElement = document.querySelector("#wind-speed");
+ let timeElement = document.querySelector("#time");
+console.log(response.data);
+let date = new Date(response.data.time*1000);
+let icon = ` <img src="https://shecodes-assets.s3.amazonaws.com/api/weather/icons/few-clouds-night.png"class ="my-temperature-app-icon"/>`;
 
-console.log(wind);
 
  cityElement.innerHTML = response.data.city;
- descriptionElement.innerHTML = response.data.condition.description;
+timeElement.innerHTML = `${date.getDay()}${date.getHours()}:${date.getMinutes()}`
 
-humidityElement.innerHTML = `${response.data.condition.humidity}%`;
+ timeElement.innerHTML = formatDate(date);
+ descriptionElement.innerHTML = response.data.condition.description;
+windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
 temperatureElement.innerHTML = Math.round(temperature);
+}
+function formatDate(date) {
+    let minutes = date.getMinutes();
+    let hours = date.getHours();
+    let days = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+    ]
+    let day = days[date.getDay()];
+     
+    if (minutes < 10) {
+        minutes = `0${minutes}`;
+    }
+
+    return `${day} ${hours}:${minutes}`;
 }
 
 
